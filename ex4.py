a8dedbc951945dab443abf0cf3643079f4381240
Eike <eike2d85@gmail.com>
2022-09-13 22:34:57 -0300

    Final_version

--- ex4.py
+++ ex4.py
@@ -3,22 +3,17 @@ import numpy as np
 def flex_stiff_plano(E1, E2, v12, G12):
     S = np.zeros((3,3))
     v21 = v12*(E2/E1)
-    
     s11 = 1/E1
     s22 = 1/E2
     s12 = -v12/E1
-
     s21 = -v21/E2
     s66 = 1/G12
-
     S.itemset((0,0),s11)
     S.itemset((1,1),s22)
     S.itemset((0,1),s12)
     S.itemset((1,0),s21)
     S.itemset((2,2),s66)
-
     C = np.linalg.inv(S)
-    
     return C,S
 
 
--- ex7.py
+++ ex7.py
@@ -1,5 +1,20 @@
 import numpy as np
-from ex4 import flex_stiff_plano
+
+def flex_stiff_plano(E1, E2, v12, G12):
+    S = np.zeros((3,3))
+    v21 = v12*(E2/E1)
+    s11 = 1/E1
+    s22 = 1/E2
+    s12 = -v12/E1
+    s21 = -v21/E2
+    s66 = 1/G12
+    S.itemset((0,0),s11)
+    S.itemset((1,1),s22)
+    S.itemset((0,1),s12)
+    S.itemset((1,0),s21)
+    S.itemset((2,2),s66)
+    C = np.linalg.inv(S)
+    return C,S
 
 # Tensões gobais em MPa
 sigma_x = 4
@@ -33,7 +48,29 @@ C_tmp1 = np.matmul(T_inv,C_loc)
 C_tmp2 = np.matmul(C_tmp1,R)
 C_tmp3 = np.matmul(C_tmp2,T)
 C_glob = np.matmul(C_tmp3,R_inv)
-print('Matriz de rigidez global [MPa]:\n', C_glob)
-
+# print('Matriz de rigidez global [MPa]:\n', C_glob)
+S_glob = np.linalg.inv(C_glob)
+# print('Matriz de fexibilidade global [1/MPa]:\n', S_glob)
+def_glob = np.matmul(S_glob,glob_ten)
+print('Deformações Globais:\n', def_glob)
 loc_ten = np.matmul(T,glob_ten)
 print('Tensões Locais [MPa]:\n', loc_ten)
+loc_ten_mod = loc_ten
+loc_ten_mod.itemset((2),loc_ten.item(2)/2)
+def_loc = np.matmul(S_loc,loc_ten_mod)
+print('Deformações Locais:\n', def_loc)
+sig_max = ((sigma_x + sigma_y)/2)+(((sigma_x - sigma_y)/2)**2 + tau_xy**2)**0.5
+sig_min = ((sigma_x + sigma_y)/2)-(((sigma_x - sigma_y)/2)**2 + tau_xy**2)**0.5
+print('Tensão Normal Principal 1 [MPa]:', sig_max)
+print('Tensão Normal Principal 2 [MPa]:', sig_min)
+tau_max = (((sigma_x - sigma_y)/2)**2 + tau_xy**2)**0.5
+print('Tensão Cisalhante Principal [MPa]:', tau_max)
+e_x = def_glob.item(0)
+e_y = def_glob.item(1)
+gamma_xy = def_glob.item(2)
+e_max = ((e_x + e_y)/2)+(((e_x - e_y)/2)**2 + gamma_xy**2)**0.5
+e_min = ((e_x + e_y)/2)-(((e_x - e_y)/2)**2 + gamma_xy**2)**0.5
+print('Deformação Normal Principal 1:', e_max)
+print('Deformação Normal Principal 2:', e_min)
+gamma_max = ((e_x-e_y)**2 + gamma_xy**2)**0.5
+print('Deformação Cisalhante Principal:', gamma_xy)
